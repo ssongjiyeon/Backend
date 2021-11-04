0b8b166cb1d876e7a36c880caa4d04543cd822a0
Jiyeon526 <b8480@naver.com>
2021-11-04 15:39:36 +0900

    [feat]: 기본 api 작성

--- Security/demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/form/SampleController.java
+++ Security/demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/form/SampleController.java
@@ -1,5 +1,7 @@
 package me.whiteship.demospringsecurityform.form;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SampleController {
 	
 	@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("message", "hello spring security"); // 키-밸류값으로 모델 값 설정 이 값을 index페이지에서 참조
+	public String index(Model model, Principal principal) {
+		if(principal == null) {
+			model.addAttribute("message", "hello spring security"); // 키-밸류값으로 모델 값 설정 이 값을 index페이지에서 참조
+		} else {
+			model.addAttribute("message", "hello index " + principal.getName());
+		}
 		
 		return "index";
+	}
+	
+	@GetMapping("/info")
+	public String info(Model model) { // 아무나 접근 가능
+		model.addAttribute("message", "Info"); 	
+		
+		return "info";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard(Model model, Principal principal) { // 인증된 사용자만 접근 가능
+		model.addAttribute("message", "hello " + principal.getName()); 
+		
+		return "dashboard";
+	}
+	
+	@GetMapping("/admin")
+	public String admin(Model model, Principal principal) { // 인증된 사용자 + 역할이 admin인 사람만 접근 가능
+		model.addAttribute("message", "hello admin " + principal.getName());
+		
+		return "admin";
 	}
 }

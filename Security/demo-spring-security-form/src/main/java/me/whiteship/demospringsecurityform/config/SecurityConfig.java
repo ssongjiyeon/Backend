de184d74347a9ad21b4c2bc7bfbda00a337f73ba
Jiyeon526 <b8480@naver.com>
2021-11-05 11:19:43 +0900

    [refactor]: 유저 부분 주석처리

--- Security/demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ Security/demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -1,19 +1,25 @@
 package me.whiteship.demospringsecurityform.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import me.whiteship.demospringsecurityform.account.AccountService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+//	@Autowired
+//	AccountService accountService; // 이걸 명시적으로 선언
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.mvcMatchers("/", "/info").permitAll()
+				.mvcMatchers("/", "/info", "/account/**").permitAll()
 				.mvcMatchers("/admin").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				.and()
@@ -22,10 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.httpBasic();
 	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("Jiyeon").password("{noop}123").roles("USER").and()
-				.withUser("admin").password("{noop}!@#").roles("ADMIN");
-	}
+//	@Override // 명시적으로 선언 -> 근데 빈이 등록만 되어 있으면 알아서 갔다 써서 필요 없음
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(accountService); // 이걸 써서 유저 정보를 가져온다.
+//	}
+	
+// DB를 사용해서 인증을 하기때문에 이제 이 부분이 필요 없음
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//				.withUser("Jiyeon").password("{noop}123").roles("USER").and()
+//				.withUser("admin").password("{noop}!@#").roles("ADMIN");
+//	}
 }

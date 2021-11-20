2bf0002310e4b724e69c2ccff989f9503661bed2
Jiyeon526 <b8480@naver.com>
2021-11-21 02:56:27 +0900

    Feat: EntityManger 사용해서 DB값 생성, 조회

--- JPA/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,38 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
 	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
 		
-
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {
+			// 생성
+//			Member member = new Member();
+//			
+//			member.setId(2L);
+//			member.setName("helloa");
+			
+			// 조회
+			Member findMember = em.find(Member.class, 1L);
+			findMember.setName("helloa");
+			
+//			em.persist(findMember);
+			tx.commit();
+		} catch(Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
 	}
 
 }

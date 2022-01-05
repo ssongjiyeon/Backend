a382f254161a92dad7136dae36a5b0d883cda0b3
Jiyeon526 <b8480@naver.com>
2022-01-05 23:13:18 +0900

    Refactor: MemberServiceImpl 생성자 주입으로 변경

---
+++ Spring/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+}

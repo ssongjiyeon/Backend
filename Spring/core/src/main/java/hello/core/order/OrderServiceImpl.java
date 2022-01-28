bbffca1580540f1d73eee6df10cdfb4d9fcb14c8
Jiyeon526 <b8480@naver.com>
2022-01-29 02:09:08 +0900

    Feat: 어노테이션 만들기

--- Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -11,11 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 //    private MemberRepository memberRepository;
 //    private DiscountPolicy discountPolicy;
 //
@@ -23,6 +28,8 @@ public class OrderServiceImpl implements OrderService {
 //    public void setMemberRepository(MemberRepository memberRepository) {
 //        this.memberRepository = memberRepository;
 //    }
+
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

5e5620555def76cfd9e390109ea6987e08064ee5
Jiyeon526 <b8480@naver.com>
2022-04-05 23:55:37 +0900

    Feat: jwt 임시 토큰 만들어보기

--- Security/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ Security/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,10 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter1(), SecurityContextPersistenceFilter.class);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다.
             .and()
-            .addFilter(corsFilter)
+            .addFilter(corsFilter) // CrossOrigin 인증 x, 시큐리티 필터에 등록 인증
             .formLogin().disable()
             .httpBasic().disable()
             .authorizeRequests()
--- Security/jwt/src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ Security/jwt/src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -1,12 +1,28 @@
 package com.cos.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter1 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("필터1");
-        filterChain.doFilter(servletRequest, servletResponse);
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+       // 토큰 이름 : 코스 -> 인증
+       if(req.getMethod().equals("POST")) {
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+
+            if(headerAuth.equals("cos")) {
+                filterChain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증안됨");
+            }
+        }
     }
 }
